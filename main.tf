e311a01c6cb9ad8c31b562b1475183d9de6d64db
Patrick <krals.digital@gmail.com>
2021-05-19 10:16:04 -0300

    Adiconar banco de dados

--- main.tf
+++ main.tf
@@ -11,6 +11,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+provider "aws" {
+  alias = "us-east-2"
+  region = "us-east-2"
+}
+
 resource "aws_key_pair" "terraform_aws" {
   key_name = "terraform-aws"
   public_key = "${file("C:\\Users\\Felip\\.ssh\\terraform-aws.pub")}"
@@ -50,20 +55,18 @@ resource "aws_instance" "dev5" {
   vpc_security_group_ids = [ "${aws_security_group.acesso-ssh.id}" ]
 }
 
-resource "aws_security_group" "acesso-ssh" {
-  name        = "acesso-ssh"
-  description = "acesso-ssh"
-
-  ingress {
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["181.191.205.18/32"]
-  }
-
+resource "aws_instance" "dev6" {
+  provider = aws.us-east-2
+  ami = "ami-00399ec92321828f5"
+  instance_type = "t2.micro"
+  key_name = "${aws_key_pair.terraform_aws.key_name}"
   tags = {
-    Name = "ssh"
+    "Name" = "dev5"
   }
+  vpc_security_group_ids = [ "${aws_security_group.acesso-ssh-us-east-2.id}" ]
+  depends_on = [
+    aws_dynamodb_table.dynamodb-homologacao
+  ]
 }
 
 resource "aws_s3_bucket" "dev4" {
@@ -73,4 +76,23 @@ resource "aws_s3_bucket" "dev4" {
   tags = {
     Name = "call4fit-dev4"
   }
+}
+
+resource "aws_dynamodb_table" "dynamodb-homologacao" {
+  provider = aws.us-east-2
+  name           = "GameScores"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "UserId"
+  range_key      = "GameTitle"
+
+  attribute {
+    name = "UserId"
+    type = "S"
+  }
+
+  attribute {
+    name = "GameTitle"
+    type = "S"
+  }
+
 }
---
+++ security_group.tf
@@ -0,0 +1,32 @@
+resource "aws_security_group" "acesso-ssh" {
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["181.191.205.18/32"]
+  }
+
+  tags = {
+    Name = "ssh"
+  }
+}
+
+resource "aws_security_group" "acesso-ssh-us-east-2" {
+  provider = aws.us-east-2
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+
+  ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["181.191.205.18/32"]
+  }
+
+  tags = {
+    Name = "ssh"
+  }
+}
